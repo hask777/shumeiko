7653315cd6573ee484793bb0842f7f5546e9ddad
hask777 <haskellisp@gmail.com>
2023-07-30 18:29:02 +0300

    last

--- src/auth/models.py
+++ src/auth/models.py
@@ -1,5 +1,12 @@
+import sys
+sys.path.append("..")
+
 from datetime import datetime
-from sqlalchemy import Boolean, MetaData, Table, Column, Integer, String, TIMESTAMP, ForeignKey, JSON
+
+from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTable
+from sqlalchemy import Table, Column, Integer, String, TIMESTAMP, ForeignKey, JSON, Boolean, MetaData
+
+from src.database import Base
 
 metadata = MetaData()
 
@@ -24,3 +31,14 @@ user = Table(
     Column("is_superuser", Boolean, default=False, nullable=False),
     Column("is_verified", Boolean, default=False, nullable=False),
 )
+
+class User(SQLAlchemyBaseUserTable[int], Base):
+    id = Column(Integer, primary_key=True)
+    email = Column(String, nullable=False)
+    username = Column(String, nullable=False)
+    registered_at = Column(TIMESTAMP, default=datetime.utcnow)
+    role_id = Column(Integer, ForeignKey(role.c.id))
+    hashed_password: str = Column(String(length=1024), nullable=False)
+    is_active: bool = Column(Boolean, default=True, nullable=False)
+    is_superuser: bool = Column(Boolean, default=False, nullable=False)
+    is_verified: bool = Column(Boolean, default=False, nullable=False)
--- src/main.py
+++ src/main.py
@@ -1,47 +1,24 @@
-import sys
-sys.path.append("..")
+from fastapi import FastAPI
 
-from fastapi_users import fastapi_users, FastAPIUsers
-
-from fastapi import FastAPI, Request, status, Depends
-from fastapi.encoders import jsonable_encoder
-from fastapi.exceptions import RequestValidationError
-from fastapi.responses import JSONResponse
-
-from auth.auth import auth_backend
-from src.database import User
-from auth.manager import get_user_manager
+from auth.base_config import auth_backend, fastapi_users
 from auth.schemas import UserRead, UserCreate
+
+# from operations.router import router as router_operation
 
 app = FastAPI(
     title="Trading App"
 )
 
-fastapi_users = FastAPIUsers[User, int](
-    get_user_manager,
-    [auth_backend],
-)
-
 app.include_router(
     fastapi_users.get_auth_router(auth_backend),
-    prefix="/auth/jwt",
-    tags=["auth"],
+    prefix="/auth",
+    tags=["Auth"],
 )
 
 app.include_router(
     fastapi_users.get_register_router(UserRead, UserCreate),
     prefix="/auth",
-    tags=["auth"],
+    tags=["Auth"],
 )
 
-current_user = fastapi_users.current_user()
-
-@app.get("/protected-route")
-def protected_route(user: User = Depends(current_user)):
-    return f"Hello, {user.username}"
-
-
-
-@app.get("/unprotected-route")
-def unprotected_route():
-    return f"Hello, anonym"
+# app.include_router(router_operation)
