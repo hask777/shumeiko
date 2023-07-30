a9130ddfdf1cbc4ae72dc493404229ec6fbcbc2a
hask777 <haskellisp@gmail.com>
2023-07-30 14:53:26 +0300

    end auth with fastapi users

--- auth/database.py
+++ auth/database.py
@@ -12,7 +12,7 @@ from sqlalchemy.orm import mapped_column
 
 from config import DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER
 from models.models import role
-from time import ti
+from time import time
 
 DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 Base: DeclarativeMeta = declarative_base()
