c4c4e0dbed9f1b6f68cae98b90039855dbcf0804
hask777 <haskellisp@gmail.com>
2023-07-30 21:02:07 +0300

    redis fail

--- src/main.py
+++ src/main.py
@@ -1,5 +1,12 @@
 from fastapi import FastAPI
 
+
+from fastapi_cache import FastAPICache
+# from fastapi_cache.backends.redis import RedisBackend
+# from fastapi_cache.decorator import cache
+
+# from redis import asyncio as aioredis
+
 from auth.base_config import auth_backend, fastapi_users
 from auth.schemas import UserRead, UserCreate
 
@@ -14,7 +21,7 @@ app.include_router(
     prefix="/auth",
     tags=["Auth"],
 )
- 
+
 app.include_router(
     fastapi_users.get_register_router(UserRead, UserCreate),
     prefix="/auth",
@@ -23,3 +30,8 @@ app.include_router(
 
 app.include_router(router_operation)
 
+
+# @app.on_event("startup")
+# async def startup_event():
+#     redis = aioredis.from_url("redis://localhost", encoding="utf8", decode_responses=True)
+#     FastAPICache.init(RedisBackend(redis), prefix="fastapi-cache")
