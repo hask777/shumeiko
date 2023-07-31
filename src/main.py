afb0b2b60879d5b6e8a49e761f827d236de403df
hask777 <haskellisp@gmail.com>
2023-07-31 13:42:06 +0300

    bg tasks

--- src/main.py
+++ src/main.py
@@ -11,6 +11,7 @@ from auth.base_config import auth_backend, fastapi_users
 from auth.schemas import UserRead, UserCreate
 
 from operations.router import router as router_operation
+from tasks.router import router as router_tasks
 
 app = FastAPI(
     title="Trading App"
@@ -29,6 +30,7 @@ app.include_router(
 )
 
 app.include_router(router_operation)
+app.include_router(router_tasks)
 
 
 # @app.on_event("startup")
