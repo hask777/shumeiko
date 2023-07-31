db96fcc5da484a41942a57614e58733a1b7d3844
hask777 <haskellisp@gmail.com>
2023-07-31 19:04:43 +0300

    tamplates

--- src/main.py
+++ src/main.py
@@ -1,21 +1,21 @@
 from fastapi import FastAPI
-
-
-from fastapi_cache import FastAPICache
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+# from fastapi_cache import FastAPICache
 # from fastapi_cache.backends.redis import RedisBackend
-# from fastapi_cache.decorator import cache
-
 # from redis import asyncio as aioredis
 
 from auth.base_config import auth_backend, fastapi_users
-from auth.schemas import UserRead, UserCreate
-
+from auth.schemas import UserCreate, UserRead
 from operations.router import router as router_operation
 from tasks.router import router as router_tasks
-
+from pages.router import router as router_pages
+ 
 app = FastAPI(
     title="Trading App"
 )
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 app.include_router(
     fastapi_users.get_auth_router(auth_backend),
@@ -31,7 +31,20 @@ app.include_router(
 
 app.include_router(router_operation)
 app.include_router(router_tasks)
+app.include_router(router_pages)
 
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "OPTIONS", "DELETE", "PATCH", "PUT"],
+    allow_headers=["Content-Type", "Set-Cookie", "Access-Control-Allow-Headers", "Access-Control-Allow-Origin",
+                   "Authorization"],
+)
 
 # @app.on_event("startup")
 # async def startup_event():
